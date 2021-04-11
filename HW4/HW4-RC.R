cb9a18d6e575e7b6f9e6dd23f2fec187dc07467f
Rmcortez12 <33045009+Rmcortez12@users.noreply.github.com>
2021-04-10 21:11:50 -0500

    adding 1b

--- HW4/HW4-RC.R
+++ HW4/HW4-RC.R
@@ -1,7 +1,7 @@
-
+####1a
 #function to get phat estimate
 
-phat_f <- function(n=30,m=10000,h0=1,dist){
+phat_f <- function(n=30,m=10000,h0=1,dist, mu1 =1){
   #n - number of draws from distribution
   #m - number of samples drawn from distribution
   #h0 - null hypothesis
@@ -13,13 +13,13 @@ phat_f <- function(n=30,m=10000,h0=1,dist){
   for (j in 1:m){
     
     if(dist=="norm"){
-      data <- rnorm(n,h0,sd = sqrt(2))
+      data <- rnorm(n,mu1,sd = sqrt(2))
     }else if (dist=="chi"){
-      data <- rchisq(n,1)
+      data <- rchisq(n,mu1)
     }else if (dist=="u"){
-      data <- runif(m,0,2)
+      data <- runif(m,0,2*mu1)
     }else if (dist=="exp"){
-      data <- rexp(m,1)
+      data <- rexp(m,mu1)
     }
     ttest <- t.test(data,alternative = "two.sided",mu = h0)
     p[j] <- ttest$p.value
@@ -30,6 +30,10 @@ phat_f <- function(n=30,m=10000,h0=1,dist){
   phat.1 <- mean(p<.1)
   #add relative error and size columns
   #keep this seperate b/c return is truncating the numbers 
+  
+  #power for number 1b
+  
+  
   return(c("alpha05" = phat.05,"alpha1" = phat.1))
 }
 
@@ -52,3 +56,29 @@ exp.300 <- phat_f(300,m=10000,1,dist="exp")
 
 #put all values into a table
 empirical_alpha <- rbind(norm.30,norm.300,chi.30,chi.300,unif.30,unif.300,exp.30,exp.300)
+
+
+#####1B
+#Power is the probability of rejection given a mu
+#  p<.05 
+# re-use above function, modify to take in different mu
+
+mus <- c(seq(0,4,.25))
+l <- length(mus)
+n.power<-numeric(l)
+c.power<-numeric(l)
+u.power<-numeric(l)
+e.power<-numeric(l)
+for(i in 1:l){
+  n.power[i] <- phat_f(300,m=10000,1,dist="norm",mu1 = mus[i])["alpha05"]
+  c.power[i] <- phat_f(300,m=10000,1,dist="chi",mu1 = mus[i])["alpha05"]
+  u.power[i] <- phat_f(300,m=10000,1,dist="u",mu1 = mus[i])["alpha05"]
+  #e.power[i] <- phat_f(300,m=10000,1,dist="exp",mu1 = mus[i])["alpha05"]
+}
+for(i in 2:l){
+  e.power[i] <- phat_f(300,m=10000,1,dist="exp",mu1 = mus[i])["alpha05"]
+}
+plot(mus,n.power)
+plot(mus,c.power)
+plot(mus,u.power)
+plot(mus,e.power)
